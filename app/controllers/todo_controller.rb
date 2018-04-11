b6e2a932011b8d1a781139c5bb3b97f96547cd3c
Taiwo Ayedun <tzayedun@cps.edu>
2018-04-11 13:42:42 +0000

    Add links to todos on the index page

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,7 +3,15 @@ class TodoController < ApplicationController
     end
     
     def show
-        @todo_description = "Learn Variables"
-        @todo_pomodoro_estimate = 4
+        
+        todo_id = '2'
+  if todo_id == '1'
+    @todo_description = "Make the curriculum"
+    @todo_pomodoro_estimate = 4
+  elsif todo_id == '2'
+    @todo_description = "Buy workshop supplies"
+    @todo_pomodoro_estimate = 3
+  end
+
     end
 end
